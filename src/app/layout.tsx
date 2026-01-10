be7e58e439dc0570dcaac95772e357d203c8551f
Rickson de Oliveira Viana <rickson2013oliveira@gmail.com>
2026-01-10 20:05:26 -0300

    fix: Removendo um import não utilizado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages, getLocale } from "next-intl/server";
